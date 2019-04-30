9fbf9979e9ae5b7fb791883f8803d8eb53dae1a8
zhouzhu85 <734837014@qq.com>
2019-04-30 18:03:45 +0800

    修改

---
+++ src/main/java/com/shiro/demo/config/ShiroConfig.java
@@ -0,0 +1,13 @@
+package com.shiro.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author zhouzhu
+ */
+@Configuration
+public class ShiroConfig {
+//    @Autowired(required = false)
+//    private ResourcesService
+}
